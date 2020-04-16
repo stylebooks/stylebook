87c07ece1500f0798e8fdd2a19c158417938bdd7
alanoliv <alanbruno@gmail.com>
2020-04-16 18:16:54 -0300

    Updates

---
+++ precache-manifest.2e4861172cadd81cbb33dc643778fb78.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f828104bbf12aa57d7cdca5041c3569f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a32b2b92ad4f66e85f39.bundle.js"
+  },
+  {
+    "url": "runtime~main.a32b2b92ad4f66e85f39.bundle.js"
+  },
+  {
+    "url": "vendors~main.a32b2b92ad4f66e85f39.bundle.js"
+  },
+  {
+    "url": "vendors~main.a32b2b92ad4f66e85f39.bundle.js.LICENSE.txt"
+  }
+]);
