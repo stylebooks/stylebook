eaec9a64579c79ed109e7fa6d24727d94e3aee81
alanoliv <alanbruno@gmail.com>
2020-04-15 21:29:08 -0300

    feat(demo): adding storybook test theme

--- packages/demo/.storybook/main.js
+++ packages/demo/.storybook/main.js
@@ -3,6 +3,6 @@ module.exports = {
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
-    '@storybook/addon-links',
-  ],
+    '@storybook/addon-links'
+  ]
 };
