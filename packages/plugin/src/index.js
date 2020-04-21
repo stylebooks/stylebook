669544ede21f4d6b0f457388a4345e1dacf1ce6a
alanoliv <alanbruno@gmail.com>
2020-04-21 18:56:21 -0300

    feat(demo): adding plugin as depedency

---
+++ packages/plugin/src/index.js
@@ -0,0 +1,5 @@
+import { addons } from '@storybook/addons';
+
+addons.register('StyleBook', (api) => {
+  console.log('stylebook registered');
+});
--- packages/plugin/src/plugin.js
+++
@@ -1,5 +0,0 @@
-import addons from '@storybook/addons';
-
-addons.register('StyleBook', (api) => {
-  console.log('stylebook registered');
-});
