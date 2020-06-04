eff89823f4d6ba00344e3bbbc45e4b7d7248027f
alanoliv <alan.oliveira@gympass.com>
2020-06-04 19:54:05 -0300

    feat(again): another test

--- packages/addon/src/constants.js
+++ packages/addon/src/constants.js
@@ -2,6 +2,5 @@ const ADDON_ID = 'stylebookPanel';
 const PARAM_KEY = 'stylebookAddon';
 const PANEL_ID = `${ADDON_ID}/panel`;
 const PANEL_TITLE = 'Stylebook';
-const PANEL_TITLE = 'Stylebook';
 
 export { ADDON_ID, PARAM_KEY, PANEL_ID, PANEL_TITLE };
