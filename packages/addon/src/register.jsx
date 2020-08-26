589f320334417270683675f10cf9bed297fdd3f2
alanoliv <alan.oliveira@gympass.com>
2020-08-26 12:34:34 -0300

    feat(addon): change theme via addon api

--- packages/addon/src/register.jsx
+++ packages/addon/src/register.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { addons, types } from '@storybook/addons';
 import { ADDON_ID, PANEL_ID } from './constants';
 
-const StylebookPanel = () => <div>stylebook panel</div>;
+import PurpleRain from '@stylebook/themes';
+
+const StylebookPanel = ({ api }) => {
+  const setTheme = () => {
+    api.setOptions({
+      theme: PurpleRain,
+    });
+  };
+  return <div onClick={setTheme}>Change theme</div>;
+};
 
 addons.register(ADDON_ID, (api) =>
   addons.add(PANEL_ID, {
     title: PANEL_ID,
     type: types.TOOL,
     match: () => true,
-    render: () => <StylebookPanel />,
+    render: () => <StylebookPanel api={api} />,
   })
 );
--- packages/demo/.storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons';
-import PurpleRain from '@stylebook/themes';
-
-addons.setConfig({
-  theme: PurpleRain,
-});
