4745b972c8190ea848dae410ce39bfde92207c7a
alanoliv <alan.oliveira@gympass.com>
2020-08-27 16:54:42 -0300

    feat(purplerain): adding PurpleRain theme real colors

--- packages/themes/src/PurpleRain/theme.js
+++ packages/themes/src/PurpleRain/theme.js
@@ -1,15 +1,15 @@
 import { create } from '@storybook/theming/create';
 
 const PurpleRain = create({
-  base: 'light',
+  base: 'dark',
 
-  colorPrimary: 'hotpink',
-  colorSecondary: 'deepskyblue',
+  colorPrimary: '#1E2132',
+  colorSecondary: '#171928',
 
   // UI
-  appBg: 'white',
-  appContentBg: 'silver',
-  appBorderColor: 'grey',
+  appBg: '#1E2132',
+  appContentBg: '#303348',
+  appBorderColor: '#5A5F86',
   appBorderRadius: 4,
 
   // Typography
@@ -17,23 +17,23 @@ const PurpleRain = create({
   fontCode: 'monospace',
 
   // Text colors
-  textColor: 'black',
-  textInverseColor: 'rgba(255,255,255,0.9)',
+  textColor: '#B1B5D5',
+  textInverseColor: '#B1B5D5',
 
   // Toolbar default and active colors
-  barTextColor: 'silver',
-  barSelectedColor: 'black',
-  barBg: 'hotpink',
+  barTextColor: '#B1B5D5',
+  barSelectedColor: 'red',
+  barBg: '#303348',
 
   // Form colors
   inputBg: 'white',
   inputBorder: 'silver',
-  inputTextColor: 'black',
+  inputTextColor: '#B1B5D5',
   inputBorderRadius: 4,
-
-  brandTitle: 'My custom storybook',
-  brandUrl: 'https://example.com',
-  brandImage: 'https://placehold.it/350x150',
 });
+
+// brandTitle: 'My custom storybook',
+// brandUrl: 'https://example.com',
+// brandImage: 'https://placehold.it/350x150',
 
 export default PurpleRain;
