4b79aef5d757f83cb993f0a9c5d3068d9e3f87d3
alanoliv <alan.oliveira@gympass.com>
2020-08-28 17:17:23 -0300

    feat(colors): purpleRain theme colors

---
+++ packages/themes/src/PurpleRain/colors.js
@@ -0,0 +1,29 @@
+const colors = {
+  base: {
+    light: 'light',
+    dark: 'dark',
+  },
+  main: {
+    primary: '#303348',
+    secondary: '#171928',
+    background: '#1E2132',
+    border: '#5A5F86',
+    text: '#B1B5D5',
+    invertedText: '#171928',
+  },
+  preview: {
+    background: '#303348',
+  },
+  toolbar: {
+    background: '#303348',
+    text: '#B1B5D5',
+    selectedText: '#B452C8',
+  },
+  input: {
+    background: '#FFFFFF',
+    border: '#F4F4F4',
+    text: '#B1B5D5',
+  },
+};
+
+export default colors;
--- packages/themes/src/PurpleRain/theme.js
+++ packages/themes/src/PurpleRain/theme.js
@@ -1,39 +1,4 @@
-import { create } from '@storybook/theming/create';
+import { withStyleBookTheme } from '../utils';
+import colors from './colors';
 
-const PurpleRain = create({
-  base: 'dark',
-
-  colorPrimary: '#1E2132',
-  colorSecondary: '#171928',
-
-  // UI
-  appBg: '#1E2132',
-  appContentBg: '#303348',
-  appBorderColor: '#5A5F86',
-  appBorderRadius: 4,
-
-  // Typography
-  fontBase: '"Open Sans", sans-serif',
-  fontCode: 'monospace',
-
-  // Text colors
-  textColor: '#B1B5D5',
-  textInverseColor: '#B1B5D5',
-
-  // Toolbar default and active colors
-  barTextColor: '#B1B5D5',
-  barSelectedColor: 'red',
-  barBg: '#303348',
-
-  // Form colors
-  inputBg: 'white',
-  inputBorder: 'silver',
-  inputTextColor: '#B1B5D5',
-  inputBorderRadius: 4,
-});
-
-// brandTitle: 'My custom storybook',
-// brandUrl: 'https://example.com',
-// brandImage: 'https://placehold.it/350x150',
-
-export default PurpleRain;
+export default withStyleBookTheme(colors);
