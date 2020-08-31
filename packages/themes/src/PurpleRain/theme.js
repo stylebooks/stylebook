17618a1d3001d1f52d462ff88095e1c8d5b3d472
alanoliv <alanbruno@gmail.com>
2020-08-31 10:20:50 -0300

    refactor(theme): theme using tokens

--- packages/themes/src/PurpleRain/theme.js
+++ packages/themes/src/PurpleRain/theme.js
@@ -1,32 +1,31 @@
 import { create } from '@storybook/theming/create';
 import { base, fonts, radii } from '../tokens';
-import colors from './colors';
-
-const { purple, gray } = colors;
+import { gray, blue } from '../tokens/colors';
 
 const theme = {
   base: base.dark,
 
-  colorPrimary: purple.darkebonyclay,
-  colorSecondary: purple.mirage,
-  appBg: purple.darkebonyclay,
-  appBorderColor: purple.comet,
-  textColor: purple.lavender,
-  textInverseColor: purple.mirage,
+  colorPrimary: blue.zircon.i900,
+  colorSecondary: blue.darkzircon.i900,
 
-  fontBase: `${fonts.kronaone}, ${fonts.sansserif}`,
-  fontCode: fonts.monospace,
-
-  appContentBg: purple.ebonyclay,
+  appBg: blue.zircon.i900,
+  appContentBg: blue.titanwhite.i900,
+  appBorderColor: blue.athensgrey.i400,
   appBorderRadius: radii.medium,
 
-  barTextColor: purple.lavender,
-  barSelectedColor: magenta.amethyst,
-  barBg: purple.ebonyclay,
+  fontBase: `${fonts.kronaone}, ${fonts.sansserif}'`,
+  fontCode: fonts.monospace,
 
-  inputBg: gray.white,
-  inputBorder: gray.silver,
-  inputTextColor: purple.mirage,
+  textColor: blue.snuff.i100,
+  textInverseColor: blue.snuff.i100,
+
+  barTextColor: blue.snuff.i100,
+  barSelectedColor: blue.snuff.i100,
+  barBg: blue.titanwhite.i900,
+
+  inputBg: gray.alabaster.i50,
+  inputBorder: gray.alabaster.i200,
+  inputTextColor: blue.snuff.i100,
   inputBorderRadius: radii.medium,
 };
 
