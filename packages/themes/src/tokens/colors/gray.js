b959bfd6c7e5d0044ddc3cc06caf23fc18a91245
alanoliv <alanbruno@gmail.com>
2020-08-31 10:19:57 -0300

    refactor(tokens): refactoring token structure

---
+++ packages/themes/src/tokens/colors/gray.js
@@ -0,0 +1,18 @@
+import { withShades } from '../../hoc';
+
+const gray = {
+  alabaster: [
+    '#FAFAFA',
+    '#F5F5F5',
+    '#EEEEEE',
+    '#E0E0E0',
+    '#BDBDBD',
+    '#9E9E9E',
+    '#757575',
+    '#616161',
+    '#424242',
+    '#212121',
+  ],
+};
+
+export default withShades(gray);
--- packages/themes/src/tokens/index.js
+++ packages/themes/src/tokens/index.js
@@ -1,6 +1,5 @@
-import colors from './colors';
 import fonts from './fonts';
 import radii from './radii';
 import base from './base';
 
-export { colors, fonts, radii, base };
+export { fonts, radii, base };
