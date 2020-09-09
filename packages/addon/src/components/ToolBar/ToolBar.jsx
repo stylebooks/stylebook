46a4d69154cc3aa79aa6c74875f422588723f1e9
alanoliv <alanbruno@gmail.com>
2020-09-09 17:40:11 -0300

    refactor(toolbar): removing logic from toolbar and opening space for more buttons

--- packages/addon/src/components/ToolBar/ToolBar.jsx
+++ packages/addon/src/components/ToolBar/ToolBar.jsx
@@ -1,34 +1,6 @@
-import React, { useEffect } from 'react';
-import { useGlobalTypes } from '@storybook/api';
+import React, { memo } from 'react';
+import ThemeSwitcher from './ThemeSwitcher';
 
-const components = {
-  single: null,
-  toggle: <div>a</div>,
-  multi: <div>b</div>,
-};
+const ToolBar = ({ api }) => <ThemeSwitcher api={api} />;
 
-const ToolBar = ({ api }) => {
-  const { stylebook: { mode = 'single', theme, logo } = {} } = useGlobalTypes();
-
-  const setTheme = () => {
-    if (logo) {
-      const { img, title, url } = logo;
-
-      theme.brandImage = img;
-      theme.brandTitle = title;
-      theme.brandUrl = url;
-    }
-
-    api.setOptions({
-      theme: theme,
-    });
-  };
-
-  useEffect(() => {
-    theme && mode === 'single' && setTheme();
-  });
-
-  return theme ? <>{components[mode]}</> : null;
-};
-
-export default ToolBar;
+export default memo(ToolBar);
