ced7117f6f60034275365781e9d100582a3d416d
alanoliv <alanbruno@gmail.com>
2020-09-09 17:41:28 -0300

    chore(preview): removing logo and splash config from preview

--- packages/demo/.storybook/preview.js
+++ packages/demo/.storybook/preview.js
@@ -7,11 +7,6 @@ export const globalTypes = {
   stylebook: {
     mode: 'single',
     theme: PurpleRain,
-    logo: {
-      img: 'https://placehold.it/350x150',
-      title: 'Stylebook',
-      url: '/',
-    },
   },
 };
 
