2241155ba52c2019e412a23c53f57d0166aebb09
alan-oliv <alanbruno@gmail.com>
2021-09-09 21:52:47 -0300

    refactor(themeswitcher): destructuring at right level

--- packages/addon/src/components/ToolBar/Themifier/ThemeSwitcher.jsx
+++ packages/addon/src/components/ToolBar/Themifier/ThemeSwitcher.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import Switch from '../Switch';
 
-const ThemeSwitcher = ({
-  config: {
-    themes: { length },
-  },
-}) => (length > 1 ? <Switch /> : null);
+const ThemeSwitcher = ({ config: { themes } }) =>
+  themes.length > 1 ? <Switch /> : null;
 
 export default ThemeSwitcher;
